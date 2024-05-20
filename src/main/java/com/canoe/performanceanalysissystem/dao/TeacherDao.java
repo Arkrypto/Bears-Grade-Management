399d8bf15f90e7c228908f7f8ac5c0e2f256a668
canoe <northboat@163.com>
2024-05-20 20:14:23 +0800

    update to jdk17 & springboot 3.x

---
+++ src/main/java/com/canoe/performanceanalysissystem/dao/TeacherDao.java
@@ -0,0 +1,21 @@
+package com.canoe.performanceanalysissystem.dao;
+
+import com.canoe.performanceanalysissystem.pojo.Teacher;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TeacherDao {
+    private static Map<String, Teacher> teachers = null;
+
+    static{
+        teachers = new HashMap<>();
+        teachers.put("NorthBoat", new Teacher("NorthBoat", "011026"));
+        teachers.put("hahaha", new Teacher("hahaha", "123456"));
+        teachers.put("sad", new Teacher("sad", "123456"));
+    }
+
+    public static Map getTeachers(){
+        return teachers;
+    }
+}
