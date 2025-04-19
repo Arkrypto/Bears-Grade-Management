880ba153b8501c92a74dd156bf608125783e29e8
canoe <northboat@163.com>
2025-04-19 13:26:56 +0800

    rename java version

---
+++ backend-java/src/main/java/com/arkrypto/grademanagement/pojo/Teacher.java
@@ -0,0 +1,13 @@
+package com.arkrypto.grademanagement.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher {
+    String account;
+    String password;
+}
